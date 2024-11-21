6987f1aa84dc17ba1ccc5cd44d21f0ef364b1535
Tyler <tanderson1@hwemail.com>
2024-11-21 09:15:34 -0800

    Working Game
    Almost

---
+++ public/First Person Shooter/__settings__.js
@@ -0,0 +1,22 @@
+window.ASSET_PREFIX = "";
+window.SCRIPT_PREFIX = "";
+window.SCENE_PATH = "2119138.json";
+window.CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'deviceTypes': [`webgl2`, `webgl1`],
+    'powerPreference': "default"
+};
+window.SCRIPTS = [ 204964845, 204964851, 204964852, 204964827, 204964854 ];
+window.CONFIG_FILENAME = "config.json";
+window.INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+window.PRELOAD_MODULES = [
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/204964843/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/204965036/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/204964830/1/ammo.js', 'preload' : true},
+];
